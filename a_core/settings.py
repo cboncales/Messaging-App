ef5bca796eb287e2b69734d98979b3815b2ba2d4
cboncales <clarkboncales115@gmail.com>
2024-12-22 20:41:11 +0800

    added templates and posts

--- a_core/settings.py
+++ a_core/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'a_core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'a_core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tabily',
+        'USER': 'postgres',
+        'PASSWORD': '2001',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
